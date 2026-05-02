898dbba1c147834dd20445f4c7cc607f213f4095
revamplex <revamplex@gmail.com>
2026-05-02 14:41:10 -0400

    Update stripe-webhook.js

--- api/stripe-webhook.js
+++ api/stripe-webhook.js
@@ -1,3 +1,9 @@
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || process.env.STRIPE_SECRET_KEY_);
